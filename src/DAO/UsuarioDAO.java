ca05e04603a948b82f611df54ed7460e482d5037
kokeGG <kokehr99@gmail.com>
2021-11-19 21:54:21 -0600

    usuario y DAO

--- src/DAO/UsuarioDAO.java
+++ src/DAO/UsuarioDAO.java
@@ -1,13 +1,125 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
-/**
- *
- * @author koke
- */
+import clases.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+
+
 public class UsuarioDAO {
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
     
+    Conexion cn = new Conexion();
+    
+    
+        public boolean registarUsuario(Usuario usr)
+        {
+            con = cn.Conectar();
+            //Consulta a la BD para ingresar datos
+            String sql = "INSERT INTO Usuario(Usuario, Nombre, pass, idEstado, id_tipo) VALUES"
+                    + "(?, ?, ?, ?, ?)";
+            
+            try 
+            {
+                ps = con.prepareStatement(sql);
+            
+                ps.setString(1, usr.getUsername());
+                ps.setString(2, usr.getNombre());
+                ps.setString(3, usr.getPass());
+                ps.setInt(4, usr.getId_estado());
+                ps.setInt(5, usr.getId_tipo());
+                ps.execute();
+                return true;
+            } 
+            catch (SQLException ex) 
+            {
+                java.util.logging.Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+                return false;
+            }
+            
+        }
+        
+        public int existeUsuario(String usuario)
+        {
+            con = cn.Conectar();
+            //Consulta a la bd para verificar si el usuario existe          
+            String sql = "SELECT count (id) FROM Usuario WHERE Usuario = ?"; //cuenta cuantos registros hay con el "username"
+            
+            try 
+            {
+                ps = con.prepareStatement(sql);
+            
+                ps.setString(1, usuario);
+                ps.execute();
+                rs = ps.executeQuery();
+                
+                if (rs.next()) {
+                    return rs.getInt(1); //retorna el valor que realiza la cuenta en la consulta
+                }
+                return 1; // retornarmos 1 para que se crea que hay un usuario, en caso de algún problema
+            } 
+            catch (SQLException ex) 
+            {
+                java.util.logging.Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+                return 1;
+            }
+            
+        }
+        
+        public Usuario listarUsuarioID(String dni){
+        Usuario u = new Usuario();
+        String sql = "SELECT * FROM Usuario";
+        
+        try {
+            con = cn.Conectar();
+            ps = con.prepareStatement(sql);
+            while (rs.next()) {
+                u.setId(rs.getInt(1));
+                u.setId_estado(rs.getInt(2));
+                u.setUsername(rs.getString(3));
+                u.setNombre(rs.getString(4));
+                u.setPass(rs.getString(6));
+                u.setId_tipo(rs.getInt(7));
+            }
+        } catch (Exception e) {
+            System.out.println("Error al listar ID de los usuarios: " + e);
+        }
+        
+        return u;
+    }
+        
+        
+        public boolean login(Usuario usr){
+            con = cn.Conectar();
+            String sql = "SELECT u.idUsuario, u.Usuario, u.pass, u.Nombre, u.id_tipo FROM Usuarios"
+                    + "AS u  WHERE Usuario = ?";
+            
+            try {
+                ps = con.prepareStatement(sql);
+            
+                ps.setString(1, usr.getUsername());
+                rs = ps.executeQuery();
+                
+                if (rs.next()) {
+                    if (usr.getPass().equals(rs.getString(3))) { //comparamos si la contraseña es igual al de nuestra BD
+                        usr.setId(rs.getInt(1));
+                        usr.setNombre(rs.getString(4));
+                        usr.setId_tipo(rs.getInt(5));
+                        return true; //cuando las contraseñas coincidan
+                    } else{
+                        return false; //cuando las contraseñas no coincidan
+                    }
+                }
+                return false;
+            } catch (SQLException ex) 
+            {
+            java.util.logging.Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+                return false;
+            }   
+        
+        }
 }
--- src/clases/Usuario.java
+++ src/clases/Usuario.java
@@ -1,13 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package clases;
 
-/**
- *
- * @author koke
- */
+
 public class Usuario {
-    
+     int id, id_estado, id_tipo;
+    String username, nombre, pass, nombrerol;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, int id_estado, int id_tipo, String username, String nombre, String pass) {
+        this.id = id;
+        this.id_estado = id_estado;
+        this.id_tipo = id_tipo;
+        this.username = username;
+        this.nombre = nombre;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_estado() {
+        return id_estado;
+    }
+
+    public void setId_estado(int id_estado) {
+        this.id_estado = id_estado;
+    }
+
+    public int getId_tipo() {
+        return id_tipo;
+    }
+
+    public void setId_tipo(int id_tipo) {
+        this.id_tipo = id_tipo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getNombrerol() {
+        return nombrerol;
+    }
+
+    public void setNombrerol(String nombrerol) {
+        this.nombrerol = nombrerol;
+    }
 }
